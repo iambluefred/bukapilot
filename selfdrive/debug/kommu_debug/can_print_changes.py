93ecb8fe1a978a937fa3782e21a919df77503f49
iXcess <brandonting@hotmail.com>
2021-09-08 22:26:56 +0800

    Add bitfield checking

--- selfdrive/debug/kommu_debug/can_print_changes.py
+++ selfdrive/debug/kommu_debug/can_print_changes.py
@@ -7,16 +7,23 @@ import cereal.messaging as messaging
 from common.realtime import sec_since_boot
 
 
-def can_printer(bus=0):
+def can_printer(bus=0, wait_time = 10):
   """Collects messages and prints when a new bit transition is observed.
   This is very useful to find signals based on user triggered actions, such as blinkers and seatbelt.
   """
+
+  static_msgs = ["handbrake","driver door","passenger door","back left door","back right door", \
+      "seatbelt","left signal","right signal","brake","gas","generic toggle","steering"]
+  msg_c = 0
+  started = False
 
   logcan = messaging.sub_sock('can')
 
   low_to_high = defaultdict(int)
   high_to_low = defaultdict(int)
   prev = defaultdict(int)
+  found = defaultdict(set)
+  last_known = None
 
   while 1:
     can_recv = messaging.drain_sock(logcan, wait_for_one=True)
@@ -28,20 +35,41 @@ def can_printer(bus=0):
           h_l = high_to_low[y.address]
           p = prev[y.address]
 
+          changes = None
           if (i | l_h) != l_h:
             low_to_high[y.address] = i | l_h
-            output = p & (~i & 2**64-1)
-            print(f"{sec_since_boot():.2f}\t{hex(y.address)} ({y.address})\t+{bin(output)}")
+            output_lh = p & (~i & 2**64-1)
+            changes = True
 
           if (~i | h_l) != h_l:
             high_to_low[y.address] = ~i | h_l
-            output = i & (~p & 2**64-1)
-            print(f"{sec_since_boot():.2f}\t{hex(y.address)} ({y.address})\t-{bin(output)}")
+            output_hl = i & (~p & 2**64-1)
+            changes = True
+
+          if changes:
+            last_known = sec_since_boot()
+            if started:
+              found[last_known] = [static_msgs[msg_c-1], y.address, output_lh, output_hl]
+              print(f"{sec_since_boot():.2f}\t{hex(y.address)} ({y.address})\t-{bin(output_hl)} +{bin(output_lh)}")
+
+          # to make sure ignition on so there is the first message coming in
+          if last_known:
+            # time related logic
+            if sec_since_boot() - last_known > wait_time:
+              last_known = sec_since_boot()
+              started = True
+              print(f"Please engage then release the {static_msgs[msg_c]} within {wait_time}s.")
+              msg_c += 1
+
+          if msg_c == len(static_msgs):
+            print(found)
+            return
 
           prev[y.address] = i
 
 if __name__ == "__main__":
-  print("Usage: Leave the script running until no new transitions are seen. Then perform the action.")
+  print("Usage: Leave the script running until no new transitions are seen. This might take awhile.")
+  print("Waiting for CAN Bus to get stable")
 
   if len(sys.argv) > 1:
     can_printer(int(sys.argv[1]))
