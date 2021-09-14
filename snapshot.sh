ed9fc11cd3e0c868813a21837f9393d706fa9632
Ben Masato <benmasato@kommu.ai>
2021-09-14 17:10:04 +0800

    Add kommu snapshot script

---
+++ snapshot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "[Usage] Add the --release flag to merge for release"
+BRANCHES=(tool integration ports readme flip tempfix)
+RELEASE_OPT=$@
+
+git checkout snapshot
+
+for b in "${BRANCHES[@]}"
+do
+    git fetch origin $b:$b
+done
+
+git fetch origin base:base
+git reset base --hard
+
+for b in "${BRANCHES[@]}"
+do
+    if [ "$RELEASE_OPT" = "--release" ] && [ "$b" = "tool" ]; then
+    	continue
+    fi
+    git merge --squash $b && git commit --message "Merge '$b'"
+done
