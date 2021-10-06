b3302a2619158ee31667afccdcaced6f18af5122
Ben Masato <benmasato@kommu.ai>
2021-10-06 12:26:06 +0800

    Partially revert 1173dc06, unpatch lane_planner.py
    Camera offset in lane_planner.py will be set in `flip` branch instead.

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -8,8 +8,8 @@ from cereal import log
 TRAJECTORY_SIZE = 33
 # camera offset is meters from center car to camera
 if EON:
-  CAMERA_OFFSET = -0.12
-  PATH_OFFSET = -0.12
+  CAMERA_OFFSET = 0.06
+  PATH_OFFSET = 0.0
 elif TICI:
   CAMERA_OFFSET = -0.04
   PATH_OFFSET = -0.04
