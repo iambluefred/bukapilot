ec0b92eaa032e79fea5b4ca404f40369ad8302d3
Ben Masato <benmasato@kommu.ai>
2022-01-27 00:51:48 +0800

    Revamp RSJ logic
    - Dongle can now attempt to register via public endpoint
    - Change Karams store location to prevent being overwritten
    - Change password logic

--- selfdrive/athena/runescapej.py
+++ selfdrive/athena/runescapej.py
@@ -1,4 +1,4 @@
-from common.kommu import Karams, KRATOS_BASE, KRATOS_ADMIN
+from common.kommu import AuthException, Karams, WEB_BASE, kapi
 
 from Crypto import Random
 from Crypto.Hash import SHA224
@@ -8,48 +8,51 @@ from base64 import b64encode
 
 import json
 
-
 def register_user(imei, serial):
-  init = requests.get(KRATOS_BASE + "/self-service/registration/api")
+  karams = Karams()
+
+  # make sure the algo is the same with rsj!
+  dongle_id = SHA224.new(data=(imei + serial).encode()).hexdigest()[:16]
+  karams.put("rsj_dongle", dongle_id)
+
+  # first, try to login, WE MUST BE ABLE TO LOGIN IF DONGLE EXISTS
+  # TODO: review threat model
+  auth = True
+  try:
+    resp = kapi(requests.get, WEB_BASE + "/sessions/whoami")
+  except AuthException:
+    auth = False
+  if auth:
+    return dongle_id
+
+  init = requests.get(WEB_BASE + "/self-service/registration/api")
   if init.status_code != 200:
     raise Exception("can't init kratos flow")
-
-  di_raw = SHA224.new(data=(imei + serial).encode()).hexdigest()
-  dongle_id = di_raw[:16]
 
   data = {
       "method": "password",
       "traits.username": dongle_id,
       "traits.email": f"dongle_{dongle_id}@kommu.ai",
-      "password": b64encode(Random.get_random_bytes(12)).decode(),
+      "password": imei + serial,
   }
 
   resp = requests.post(init.json()["ui"]["action"], data=data)
-  # when 400, assume exists and try to update schema
+  # when 400, assume previously incomplete registration and try to update schema
   if resp.status_code not in (200, 400):
     return None
 
-  karams = Karams()
   rr = resp.json()
   if resp.status_code == 200:
-    ident = rr["identity"]["id"]
-    karams.put("rsj_password", data["password"])
-    karams.put("rsj_ident", ident)
     karams.put("rsj_session", rr["session_token"])
-  else:
-    ident = karams.get("rsj_ident")
 
   data = {
-      "schema_id": "dongle",
-      "state": "active",
-      "traits": {
-        "dongle_id": dongle_id,
-      },
+    "imei": imei,
+    "serial": serial,
   }
-
-  resp = requests.put(f"{KRATOS_ADMIN}/identities/{ident}", json=data)
+  resp = kapi(requests.post, f"{WEB_BASE}/rsj/register", data=data)
   if resp.status_code != 200:
     return None
 
-  return dongle_id
+  assert resp.text == dongle_id
+  return resp.text
 
