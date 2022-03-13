33c2056e43ffe7f93652531e6608bb611a8b8327
Ben Masato <benmasato@posteo.jp>
2022-03-13 15:36:02 +0800

    Update snapshot script

--- snapshot.sh
+++ snapshot.sh
@@ -1,16 +1,20 @@
-BRANCHES=(integration ports readme flip tempfix)
-
-git checkout snapshot
+#!/bin/bash
+echo "[Usage] Add the --release flag to merge for release"
+BRANCHES=(tool integration ports readme flip tempfix)
+RELEASE_OPT=$@
 
 for b in "${BRANCHES[@]}"
 do
-    git fetch kommu $b:$b
+    git fetch origin $b:$b
 done
 
-git fetch kommu base:base
+git fetch origin base:base
 git reset base --hard
 
 for b in "${BRANCHES[@]}"
 do
+    if [ "$RELEASE_OPT" = "--release" ] && [ "$b" = "tool" ]; then
+    	continue
+    fi
     git merge --squash $b && git commit --message "Merge '$b'"
 done
