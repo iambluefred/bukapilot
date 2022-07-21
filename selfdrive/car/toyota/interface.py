d6c660213512c2fb4f768617f8dc5e3350905039
Comma Device <device@comma.ai>
2022-07-21 04:58:51 +0000

    Lower down P value

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -328,7 +328,9 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.deadzoneBP = [0., 8.05]
       ret.longitudinalTuning.deadzoneV = [.0, .14]
       ret.longitudinalTuning.kpBP = [0., 5., 20.]
-      ret.longitudinalTuning.kpV = [1.3, 1.0, 0.7]
+      # original
+      # ret.longitudinalTuning.kpV = [1.3, 1.0, 0.7]
+      ret.longitudinalTuning.kpV = [0.5, 0.5, 0.5]
       ret.longitudinalTuning.kiBP = [0., 5., 12., 20., 27.]
       ret.longitudinalTuning.kiV = [.35, .23, .20, .17, .1]
       ret.stoppingBrakeRate = 0.1  # reach stopping target smoothly
