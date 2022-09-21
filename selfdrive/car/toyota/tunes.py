1a0ecbdfc1acd53d492b721eb5adddf6b0beb496
iXcess <brandonting@hotmail.com>
2022-09-21 10:40:43 +0000

    Toyota revamp and Cross Hybrid Support

--- selfdrive/car/toyota/tunes.py
+++ selfdrive/car/toyota/tunes.py
@@ -33,9 +33,9 @@ def set_long_tune(tune, name):
     tune.deadzoneBP = [0., 8.05]
     tune.deadzoneV = [.0, .14]
     tune.kpBP = [0., 5., 20.]
-    tune.kpV = [1.3, 1.0, 0.7]
+    tune.kpV = [4.6, 3.4, 2.5]
     tune.kiBP = [0., 5., 12., 20., 27.]
-    tune.kiV = [.35, .23, .20, .17, .1]
+    tune.kiV = [.53, .35, .30, .26, .15]
   # Default longitudinal tune
   elif name == LongTunes.TSS:
     tune.deadzoneBP = [0., 9.]
