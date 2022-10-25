fc0e6227d3b32bce3d7eb8027aaa2c5736b1f946
iXcess <brandonting@hotmail.com>
2022-10-25 06:27:40 +0000

    X50 Fixes

--- selfdrive/car/proton/carcontroller.py
+++ selfdrive/car/proton/carcontroller.py
@@ -5,6 +5,7 @@ from selfdrive.car.proton.values import CAR, DBC, BRAKE_SCALE, GAS_SCALE
 from opendbc.can.packer import CANPacker
 from common.numpy_fast import clip, interp
 from common.realtime import DT_CTRL
+from common.params import Params
 import cereal.messaging as messaging
 
 def apply_acttr_steer_torque_limits(apply_torque, apply_torque_last, LIMITS):
@@ -17,10 +18,6 @@ def apply_acttr_steer_torque_limits(apply_torque, apply_torque_last, LIMITS):
                         min(apply_torque_last + LIMITS.STEER_DELTA_DOWN, LIMITS.STEER_DELTA_UP))
 
   return int(round(float(apply_torque)))
-
-def compute_gb(accel):
-  gb = float(accel) / 4.0
-  return clip(gb, 0.0, 1.0), clip(-gb, 0.0, 1.0)
 
 class CarControllerParams():
   def __init__(self, CP):
@@ -39,19 +36,21 @@ class CarController():
     self.steering_direction = False
     self.params = CarControllerParams(CP)
     self.packer = CANPacker(DBC[CP.carFingerprint]['pt'])
-    self.last_resume_frame = 0
+    self.disable_radar = Params().get_bool("DisableRadar")
 
   def update(self, enabled, CS, frame, actuators, lead_visible, rlane_visible, llane_visible, pcm_cancel, ldw):
     can_sends = []
+
+    # tester present - w/ no response (keeps radar disabled)
+    if CS.CP.openpilotLongitudinalControl and self.disable_radar:
+      if (frame % 10) == 0:
+        can_sends.append([0x7D0, 0, b"\x02\x3E\x80\x00\x00\x00\x00\x00", 0])
 
     # steer
     steer_max_interp = interp(CS.out.vEgo, self.params.STEER_BP, self.params.STEER_LIM_TORQ)
     new_steer = int(round(actuators.steer * steer_max_interp))
     apply_steer = apply_acttr_steer_torque_limits(new_steer, self.last_steer, self.params)
     self.steer_rate_limited = (new_steer != apply_steer) and (apply_steer != 0)
-
-    # gas, brake
-    apply_gas, apply_brake = compute_gb(actuators.accel)
 
     ts = frame * DT_CTRL
 
@@ -61,11 +60,11 @@ class CarController():
     # CAN controlled lateral running at 50hz
     if (frame % 2) == 0:
       can_sends.append(create_can_steer_command(self.packer, apply_steer, enabled, (frame/2) % 16))
-      can_sends.append(create_hud(self.packer, apply_steer, enabled, ldw, rlane_visible, llane_visible))
-      can_sends.append(create_lead_detect(self.packer, lead_visible, enabled))
+      #can_sends.append(create_hud(self.packer, apply_steer, enabled, ldw, rlane_visible, llane_visible))
+      #can_sends.append(create_lead_detect(self.packer, lead_visible, enabled))
 
     if CS.out.cruiseState.standstill and enabled:
-      # Spam resume button to resume from standstill at max freq of 10 Hz
+      # Spam resume button to resume from standstill at max freq of 10 Hz.
       can_sends.append(send_buttons(self.packer, frame % 16))
 
     self.last_steer = apply_steer
