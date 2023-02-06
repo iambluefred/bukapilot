d847f795283160b65fe2d8d9948fc13276870a1f
iXcess <brandonting@hotmail.com>
2023-02-06 10:31:09 +0000

    bare minimal buildable
    All panda related files are deleted leaving it bare minimal (almost).
    It is buildable based on stm32f413.

--- panda/board/build_all.sh
+++ panda/board/build_all.sh
@@ -2,5 +2,3 @@
 set -e
 
 scons -u
-PEDAL=1 scons -u
-PEDAL=1 PEDAL_USB=1 scons -u
--- panda/board/stm32fx/startup_stm32f413xx.s
+++ panda/board/stm32fx/startup_stm32f413xx.s
@@ -78,7 +78,6 @@ defined in linker script */
   .type  Reset_Handler, %function
 Reset_Handler:
   ldr   sp, =_estack       /* set stack pointer */
-  bl __initialize_hardware_early
 
 /* Copy the data segment initializers from flash to SRAM */
   movs  r1, #0
