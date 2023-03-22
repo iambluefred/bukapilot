b6be224075b745afa774d7e2eb4059a06cbb3fa9
iXcess <brandonting@hotmail.com>
2023-03-22 07:07:27 +0000

    get rlogs

--- selfdrive/loggerd/uploader.py
+++ selfdrive/loggerd/uploader.py
@@ -71,7 +71,7 @@ class Uploader():
     self.last_filename = ""
 
     self.immediate_folders = ["crash/", "boot/"]
-    self.immediate_priority = {"qlog.bz2": 0, "qcamera.ts": 1}
+    self.immediate_priority = {"rlog.bz2": 0, "qcamera.ts": 1}
 
   def get_upload_sort(self, name):
     if name in self.immediate_priority:
