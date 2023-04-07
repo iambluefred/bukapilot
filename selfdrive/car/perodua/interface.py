7e143401a5927bd5670591169ffbc4a8bd2c5c8f
iXcess <brandonting@hotmail.com>
2023-04-07 03:19:18 +0000

    Retune MPC

--- selfdrive/car/perodua/interface.py
+++ selfdrive/car/perodua/interface.py
@@ -111,14 +111,14 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = ret.wheelbase * 0.44
       tire_stiffness_factor = 0.9871
       ret.mass = 1025. + STD_CARGO_KG
-      ret.wheelSpeedFactor = 1.32
+      ret.wheelSpeedFactor = 1.303
 
       ret.lateralTuning.pid.kiV, ret.lateralTuning.pid.kpV = [[0.12], [0.20]]
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0.], [255]]
       ret.lateralTuning.pid.kf = 0.00012
 
       ret.longitudinalTuning.kpBP = [0., 5., 20.]
-      ret.longitudinalTuning.kpV = [0.6, 0.55, 0.4]
+      ret.longitudinalTuning.kpV = [0.5, 0.5, 0.4]
       ret.longitudinalActuatorDelayLowerBound = 0.42
       ret.longitudinalActuatorDelayUpperBound = 0.60
 
@@ -162,7 +162,7 @@ class CarInterface(CarInterfaceBase):
 
     if candidate in ACC_CAR:
       ret.longitudinalTuning.kiBP = [5, 7, 28]
-      ret.longitudinalTuning.kiV = [0.15, 0.10, 0.06]
+      ret.longitudinalTuning.kiV = [0.12, 0.10, 0.06]
 
       ret.minEnableSpeed = -1
       ret.steerActuatorDelay = 0.30           # Steering wheel actuator delay in seconds
