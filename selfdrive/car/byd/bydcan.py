0fd4b0ae512836c55355cb144f0407faf582d04e
iXcess <brandonting@hotmail.com>
2023-08-07 10:47:14 +0800

    Update bydcan.py

--- selfdrive/car/byd/bydcan.py
+++ selfdrive/car/byd/bydcan.py
@@ -33,13 +33,13 @@ def create_can_steer_command(packer, steer_angle, steer_req, wheel_touch_warning
   values = {
     "TORQUE": steer_rate,    # TODO find out if this is really steer rate
     "STEER_REQ": steer_req,                # Try 0x2B
+    "STEER_REQ_ACTIVE_LOW": not steer_req,
     "STEER_ANGLE": steer_angle,
     "SET_ME_X01": 1 if steer_req else 0,   # Try 0x1
     "SET_ME_XEB": r if steer_req else 0, # Try 0xB
     "COUNTER": raw_cnt,
     "SET_ME_FF": 0xFF,
     "SET_ME_F": 0xF,
-    "SET_ME_X01": 1,
     "SET_ME_1_1": 1,
     "SET_ME_1_2": 1,
     }
@@ -66,6 +66,9 @@ def create_accel_command(packer, accel, enabled, raw_cnt, r):
     "CMD_REQ_ACTIVE_LOW": 0 if enabled else 1,
     "ACC_REQ_NOT_STANDSTILL": enabled,
     "ACC_CONTROLLABLE_AND_ON": enabled,
+    "ACC_OVERRIDE_OR_STANDSTILL": 0,   # TODO integrate vEgo check
+    "STANDSTILL_STATE": 0, # TODO integrate vEgo check
+    "STANDSTILL_RESUME": 0, # TODO integrate buttons
   }
 
   dat = packer.make_can_msg("ACC_CMD", 0, values)[2]
